b081b4007f0c0fac77e8f7b7309d50e92383b17d
PRATYAY MUSTAFI <pratyaymustafi@gmail.com>
2023-07-12 00:15:04 +0530

    minor changes to header adapted to dark mode

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 export default function Footer(props: any) {
 	return (
 		<>
-    <div className="flex flex-wrap text-center items-center justify-center ">
+    <div className="flex flex-wrap text-center items-center justify-center">
                 {social.map((item, index) => (
-                    <div key={index} className="mr-4 mb-2.5 mt-2.5 transform-gpu transition-all hover:scale-125" style={{ marginRight: "10px" }}>
+                    <div key={index} className="mr-5 mb-2.5 mt-2.5 transform-gpu transition-all hover:scale-125" style={{ marginRight: "10px" }}>
                         <a href={item.link} target="_blank" rel="noreferrer">
                             <Image
                                 src={item.image}
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -8,12 +8,8 @@ type Props = {
 export default function Layout({ children }: Props) {
 	return (
 		<div
-<<<<<<< HEAD
 			className="flex flex-col min-h-screen container mx-auto md:w-11/12 lg:w-full
     divide-y"
-=======
-			className="flex flex-col min-h-screen container mx-auto md:w-11/12  lg:w-full bg-background text-text"
->>>>>>> 32570c2bfad84c8cb93b7d145e039369952a6f2c
 		>
 			<div className=" h-16 ">
 				<Header />
